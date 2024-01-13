e4215bddf714036fd87bed38dcbbae1d69967880
seungyong <seungyong20@naver.com>
2024-01-14 02:45:09 +0900

    마이페이지 나의 활동 완성

--- frontend/app/(mypage)/activity/page.tsx
+++ frontend/app/(mypage)/activity/page.tsx
@@ -9,9 +9,14 @@ import {
   getMyPosts,
   getMyQuestions,
 } from "@/api/board";
+
 import Board, { APIs, KindType } from "@/components/mypage/activity/board";
 import { NavItem } from "@/components/mypage/activity/subNavigation";
 import Content from "@/components/mypage/content";
+
+import styles from "./activity.module.scss";
+import { notosansBold } from "@/styles/_font";
+import { useEffect, useState } from "react";
 
 const algorithmKindType: KindType = {
   question: "question",
@@ -69,10 +74,62 @@ const communityApis: APIs = {
   [communityKindType.favorite]: getMyFavorites,
 };
 
+type ActivityAlgorithm = {
+  correct: number;
+  inCorrect: number;
+  bookmark: number;
+};
+
 const Activity = () => {
+  const [activityAlgorithm, setActivityAlgorithm] =
+    useState<ActivityAlgorithm>();
+
+  const fetchMyAlgorithm = async () => {
+    const response = {
+      correct: 44,
+      inCorrect: 45,
+      bookmark: 30,
+    };
+
+    setActivityAlgorithm(response);
+  };
+
+  useEffect(() => {
+    fetchMyAlgorithm();
+  }, []);
+
   return (
     <>
-      <Content title="문제">문제</Content>
+      <Content title="문제">
+        <div className={styles.question}>
+          <div className={styles.item}>
+            <div
+              className={`${styles.correct} ${styles.number} ${notosansBold.className}`}
+            >
+              {activityAlgorithm ? activityAlgorithm.correct : 0}
+            </div>
+            <div>맞힌 문제</div>
+          </div>
+          <div className={styles.line}></div>
+          <div className={styles.item}>
+            <div
+              className={`${styles.incorrect} ${styles.number} ${notosansBold.className}`}
+            >
+              {activityAlgorithm ? activityAlgorithm.inCorrect : 0}
+            </div>
+            <div>시도한 문제</div>
+          </div>
+          <div className={styles.line}></div>
+          <div className={styles.item}>
+            <div
+              className={`${styles.bookmark} ${styles.number} ${notosansBold.className}`}
+            >
+              {activityAlgorithm ? activityAlgorithm.bookmark : 0}
+            </div>
+            <div>찜한 문제</div>
+          </div>
+        </div>
+      </Content>
       <Content title="알고리즘">
         <Board
           queryKey="algorithm"
