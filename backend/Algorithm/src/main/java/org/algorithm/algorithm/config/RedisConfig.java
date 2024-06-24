91265b127d2dcab0247b0970f7d36e984e784c65
seungyong <seungyong20@naver.com>
2024-06-24 22:40:30 +0900

    Redis 설정

--- backend/Algorithm/src/main/java/org/algorithm/algorithm/config/RedisConfig.java
+++ backend/Algorithm/src/main/java/org/algorithm/algorithm/config/RedisConfig.java
@@ -2,14 +2,11 @@ package org.algorithm.algorithm.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 public class RedisConfig {
