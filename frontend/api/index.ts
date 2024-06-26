2663e0022265a3a0634205974dc2f010801d1991
seungyong <seungyong20@naver.com>
2024-06-26 21:40:49 +0900

    vscode module.scss 설정
    1. typescript-plugin-modules 5.0.2 버전 또는 그 이상에서는 module.scss의 속성명을 가져오지 못하는 에러가 발생
    
    1-1. 4.2.3으로 다운그레이드

--- frontend/api/index.ts
+++ frontend/api/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const enviroment = process.env.NEXT_PUBLIC_ENVIROMENT;
 const API_URL =
   enviroment === "product"
-    ? process.env.API_URL_PRODUCT
+    ? process.env.NEXT_PUBLIC_API_URL_PRODUCT
     : process.env.NEXT_PUBLIC_API_URL_DEVELOPMENT;
 
 export const IMAGE_URL = `${API_URL}/display?filename=`;
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
+
 import path from "path";
 import { fileURLToPath } from "url";
-
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 const nextConfig = {
