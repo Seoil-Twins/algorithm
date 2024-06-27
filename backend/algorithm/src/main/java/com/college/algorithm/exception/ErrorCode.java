b7e6b0dc620b282dcfad743b333b7a6dd40b6001
seungyong <seungyong20@naver.com>
2024-06-27 18:00:08 +0900

    Custom Exception 생성

---
+++ backend/algorithm/src/main/java/com/college/algorithm/exception/ErrorCode.java
@@ -0,0 +1,33 @@
+package com.college.algorithm.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    /* 400 BAD_REQUEST : 잘못된 요청 */
+    INVALID_PARAMETER_EMAIL(HttpStatus.BAD_REQUEST, "이메일을 입력해주세요.", 40010),
+    INVALID_PARAMETER_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호를 입력해주세요.", 40020),
+
+    /* 401 UNAUTHORIZED : 잘못된 권한 */
+    INVALID_COOKIE(HttpStatus.UNAUTHORIZED, "유효하지 않은 쿠키입니다.", 40110),
+    EXPIRED_COOKIE(HttpStatus.UNAUTHORIZED, "만료된 쿠키입니다.", 40120),
+
+    /* 403 FORBIDDEN : 잘못된 접근 */
+    NOT_VERIFIED_EMAIL(HttpStatus.FORBIDDEN, "인증이 되어 있지 않은 이메일입니다.", 40310),
+
+    /* 404 NOT FOUND : 리소스 없음 */
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "사용자가 존재하지 않습니다.", 40410),
+
+    /* 409 CONFLICT : 리소스 충돌 */
+    DUPLICATE_PARAMETER_NICKNAME(HttpStatus.CONFLICT, "닉네임이 이미 존재합니다", 40910),
+    DUPLICATE_PARAMETER_EMAIL(HttpStatus.CONFLICT, "이메일이 이미 존재합니다", 40920),
+
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+    private final int errorCode;
+}
---
+++ backend/algorithm/src/main/java/com/college/algorithm/exception/ErrorResponse.java
@@ -0,0 +1,32 @@
+package com.college.algorithm.exception;
+
+import com.college.algorithm.util.CustomTimestamp;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class ErrorResponse {
+    private final LocalDateTime timestamp = new CustomTimestamp().getTimestamp();
+    private final int status;
+    private final String error;
+    private final String message;
+    private final String code;
+    private final int errorCode;
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponse.builder()
+                        .status(errorCode.getHttpStatus().value())
+                        .errorCode(errorCode.getErrorCode())
+                        .error(errorCode.getHttpStatus().name())
+                        .code(errorCode.name())
+                        .message(errorCode.getMessage())
+                        .build()
+                );
+    }
+}
